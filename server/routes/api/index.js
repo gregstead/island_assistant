be478b4648789f991852e76f1c21f72af7a0c7ef
Greg Stead <gregstead@gmail.com>
2021-02-14 09:25:06 -0600

    adds /api/users GET and POST methods

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,22 @@
-const router = require('express').Router();
-const authRoutes = require('./auth');
+const router = require("express").Router();
+const authRoutes = require("./auth");
+const db = require("../../models");
 
-router.use('/auth', authRoutes);
+router.use("/auth", authRoutes);
+
+router.get("/users", (req, res) => {
+  console.log(`I work`);
+  db.User.find().then((users) => res.json(users));
+});
+
+router.post("/users", (req, res) => {
+  console.log(`I work`);
+  db.User.create({})
+    .then((data) => {
+      console.log(`I work`);
+      res.json(data);
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
