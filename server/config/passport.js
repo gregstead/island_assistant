7c1bbf8243f51ce716cf67dcbd0dbaa629a23ebe
Greg Stead <gregstead@gmail.com>
2021-02-14 10:25:36 -0600

    makes changes for linter and express configuration

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,10 +1,26 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const { User } = require('../models');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { User } = require("../models");
 
 // Telling passport we want to use a Local Strategy.
 // In other words, we want login with a username/email and password
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
 
 // In order to help keep authentication state across HTTP requests,
 passport.serializeUser(User.serializeUser());
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,19 +1,19 @@
-const router = require("express").Router();
-const authRoutes = require("./auth");
-const db = require("../../models");
+const router = require('express').Router();
+const authRoutes = require('./auth');
+const db = require('../../models');
 
-router.use("/auth", authRoutes);
+router.use('/auth', authRoutes);
 
-router.get("/users", (req, res) => {
-  console.log(`I work`);
+router.get('/users', (req, res) => {
+  console.log('I work');
   db.User.find().then((users) => res.json(users));
 });
 
-router.post("/users", (req, res) => {
-  console.log(`I work`);
+router.post('/users', (req, res) => {
+  console.log('I work');
   db.User.create({})
     .then((data) => {
-      console.log(`I work`);
+      console.log('I work');
       res.json(data);
     })
     .catch((err) => console.log(err));
