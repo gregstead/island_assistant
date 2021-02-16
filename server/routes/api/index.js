f3c9bc8e19f4acb998ee58c1561d5a81680a6d8d
Greg Stead <gregstead@gmail.com>
2021-02-16 16:33:54 -0600

    adds remaining CRUD routes for user

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -5,12 +5,13 @@ const passport = require("passport");
 
 router.use("/auth", authRoutes);
 
-router.get("/users", (req, res) => {
-  console.log("I work");
+router.get("/user", (req, res) => {
+  // Get a list of all users
   db.User.find().then((users) => res.json(users));
 });
 
-router.post("/signup", (req, res) => {
+router.post("/user", (req, res) => {
+  // Create a new user
   db.User.create({
     username: req.body.username,
     password: req.body.password,
@@ -22,8 +23,32 @@ router.post("/signup", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.get("/user/:id", (req, res) => {
+  // Get a specific user
+  const id = req.params.id;
+  db.User.find({ _id: id }).then((data) => {
+    res.json(data);
+  });
+});
+
+router.put("/user/:id", (req, res) => {
+  // Update a specific user
+  const id = req.params.id;
+  db.User.findOneAndUpdate({ _id: id }).then((data) => {
+    res.json(data);
+  });
+});
+
+router.delete("/user/:id", (req, res) => {
+  // Delete a specific user
+  const id = req.params.id;
+  db.User.findOneAndDelete({ _id: id }).then((data) => {
+    res.json(data);
+  });
+});
+
 router.post("/login", passport.authenticate("local"), (req, res) => {
-  res.json(req.body);
+  res.json(true);
 });
 
 module.exports = router;
