0fa5c2acc39c60b9681e85118b2860fbb0609a5e
KHallam <kjhallam321@gmail.com>
2021-02-23 13:22:26 -0600

    added styling to submit buttons

---
+++ client/src/components/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function BtnStyle () {
+    return (
+<div className="buttons">
+    <button class="btn-hover color-1">SUBMIT</button>
+</div>
+)
+}
+
+export default BtnStyle;
