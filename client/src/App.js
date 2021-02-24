77d3844785d9350f9ef577db906ab44646ac6b84
Greg Stead <gregstead@gmail.com>
2021-02-23 21:48:10 -0600

    changes for protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 //import { Wrapper, Login, Register, Flowers } from "./components";
 import AppBar from "./components/Navbar/Navbar";
 //import Container from "./components/Container/Container";
@@ -7,6 +12,7 @@ import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
 import Accordion from "./pages/Home";
 import Flowers from "./pages/Flowers";
+import ProtectedRoute from "./components/ProtectedRoute";
 //import Villagers from "./components/Card/VillagerCard";
 
 function App() {
@@ -15,16 +21,19 @@ function App() {
       <div>
         <AppBar />
         {/* <Container> */}
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/home" component={Accordion} />
-            <Redirect exact from="/flowers" to="/flowers/about" />
-            <Route path="/Flowers/:page?" render={props => <Flowers {...props} />} />
-            {/* <Route exact path="/Villagers" component={Villagers} /> */}
-          </Switch>
-          {/* <Route exact path="/search" component={Search} /> */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <ProtectedRoute path="/home" component={Accordion} />
+          <Redirect exact from="/flowers" to="/flowers/about" />
+          <Route
+            path="/flowers/:page?"
+            render={(props) => <Flowers {...props} />}
+          />
+          {/* <Route exact path="/Villagers" component={Villagers} /> */}
+        </Switch>
+        {/* <Route exact path="/search" component={Search} /> */}
         {/* </Container> */}
         {/* <Footer /> */}
       </div>
