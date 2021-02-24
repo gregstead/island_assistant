54989ec2cc75e82f24a881487ee3fa9a2633e42c
Michelle McConville <mchelmconvl@gmail.com>
2021-02-24 16:25:44 -0600

    format code for Login page

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,22 +1,12 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import { Container } from "@material-ui/core";
-import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
-import Animation from "../Animation";
+import { Container, FormControl, makeStyles, TextField } from "@material-ui/core";
+import Animation from "../Animation/Animation";
+import BtnStyle from "../Button/Button";
 import API from "../../utils/API";
-import BtnStyle from '../Button/index';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: "25ch",
-  },
+  root: { display: "flex", flexWrap: "wrap" },
+  textField: { marginLeft: theme.spacing(1), marginRight: theme.spacing(1), width: "25ch" },
 }));
 
 function LoginTextFields() {
@@ -44,59 +34,59 @@ function LoginTextFields() {
   }
 
   const classes = useStyles();
-  //ref, handlechange, handlesubmit
-  // have a reference - does library allow us to reference textfield as an object
-  //
-  return (
-    <div className="login-container" style={{marginTop: '10%', display: 'block'}}>
- 
+  //ref, handleChange, handleSubmit
+  //have a reference - does library allow us to reference textfield as an object
 
-        <Animation />
-        <Container style={{ display: 'flex', 
-                            flexDirection: 'column',
-                            position: 'relative',
-                            backgroundColor: 'rgba(255,255,255,0.7)',
-                            backdropFilter: 'blur(4px)',
-                            boxShadow: '0px 10px 10px 0px grey',
-                            borderRadius: '1em',
-                            padding: '1em',
-                            marginTop: '12em' }} maxWidth="sm">
-          <FormControl >
-            <TextField
-              name="email"
-              value={loginState.email}
-              onChange={handleChange}
-              id="email"
-              label="Email"
-              placeholder="Email"
-              helperText=""
-              fullWidth margin="normal"
-              InputLabelProps={{ shrink: true }}
-              variant="outlined"
-            />
-          </FormControl>
-          <FormControl>
-            <TextField
-              name="password"
-              value={loginState.password}
-              onChange={handleChange}
-              id="password"
-              label="Password"
-              type="password"
-              placeholder="Password"
-              helperText=""
-              fullWidth margin="normal"
-              InputLabelProps={{ shrink: true }}
-              variant="outlined"
-            />
-          </FormControl>
-          <BtnStyle
-            variant="contained"
-            onClick={handleSubmit}
-          >
-          </BtnStyle>
-        </Container>
-  
+  return (
+    <div className="login-container" style={{ display: "block", marginTop: "10%" }}>
+      <Animation />
+
+      <Container maxWidth="sm"
+        style={{
+          backgroundColor: "rgba(255,255,255,0.7)",
+          backdropFilter: "blur(4px)",
+          borderRadius: "1em",
+          boxShadow: "0px 10px 10px 0px grey",
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "12em", 
+          padding: "1em",
+          position: "relative",
+        }}
+      >
+
+        <FormControl>
+          <TextField
+            id="email"
+            label="Email"
+            fullWidth margin="normal"
+            helperText=""
+            InputLabelProps={{ shrink: true }}
+            onChange={handleChange}
+            placeholder="Email"
+            value={loginState.email}
+            variant="outlined"
+          />
+        </FormControl>
+
+        <FormControl>
+          <TextField
+            id="password"
+            label="Password"
+            fullWidth margin="normal"
+            helperText=""
+            InputLabelProps={{ shrink: true }}
+            onChange={handleChange}
+            placeholder="Password"
+            type="password"
+            value={loginState.password}
+            variant="outlined"
+          />
+        </FormControl>
+
+        <BtnStyle onClick={handleSubmit} variant="contained"></BtnStyle>
+
+      </Container>
     </div>
   );
 }
