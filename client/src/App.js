3c80b0219e93a7f52b7d3913515ed7d02766d105
Michelle McConville <mchelmconvl@gmail.com>
2021-02-24 16:27:46 -0600

    code cleanup for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import MiniDrawer from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
 import Accordion from "./pages/Home";
 import Flowers from "./pages/Flowers";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Villagers from "./pages/Villagers";
-
 
 function App() {
   return (
     <Router>
       <div>
         <MiniDrawer />
-        {/* <Container> */}
+
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
           <ProtectedRoute path="/home" component={Accordion} />
-          <Redirect exact from="/flowers" to="/flowers/about" />
-          <Route
-            path="/flowers/:page?"
-            render={(props) => <Flowers {...props} />}
-          />
-          <Route exact path="/Villagers" component={Villagers} />
+          <Redirect from="/flowers" to="/flowers/about" />
+          <Route path="/flowers/:page?" render={(props) => <Flowers {...props} />} />
+          <Route path="/Villagers" component={Villagers} />
         </Switch>
         
-        {/* </Container> */}
         {/* <Footer /> */}
       </div>
     </Router>
