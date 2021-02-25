dbdd4a4e2f6f76e60e7d73d629ee871a7d19f045
Greg Stead <gregstead@gmail.com>
2021-02-25 08:38:26 -0600

    adds route for Items page and hero continer

--- client/src/pages/Items.jsx
+++ client/src/pages/Items.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Container,
+  Typography,
+  Link,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
 function Items() {
-  return <div>Items</div>;
+  const useStyles = makeStyles((theme) => ({
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+  const classes = useStyles();
+  return (
+    <div>
+      <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Swap or Drop
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              expedita placeat in dicta ab, quod magni quia iure quaerat non
+              neque velit ducimus sed debitis! Doloribus voluptatibus dolore
+              incidunt cumque?
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Items;
