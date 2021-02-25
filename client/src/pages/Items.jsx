d811004300027f7dfc08402225e541d4556af5c1
Greg Stead <gregstead@gmail.com>
2021-02-25 14:41:46 -0600

    tweaked items page

--- client/src/pages/Items.jsx
+++ client/src/pages/Items.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
-  Grid,
   Container,
-  Typography,
+  Grid,
   Link,
+  MenuItem,
+  TextField,
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
+import API from "../utils/API";
 
 function Items() {
+  const [searchState, setSearchState] = useState({
+    category: "",
+    searchTerm: "",
+  });
+
   const useStyles = makeStyles((theme) => ({
     heroContent: {
       backgroundColor: theme.palette.background.paper,
     },
+    heroSearch: {},
   }));
+
   const classes = useStyles();
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setSearchState({
+      ...searchState,
+      [name]: value,
+    });
+  }
+
   return (
     <div>
       <main>
@@ -44,6 +63,46 @@ function Items() {
               neque velit ducimus sed debitis! Doloribus voluptatibus dolore
               incidunt cumque?
             </Typography>
+            <div className={classes.heroSearch}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <TextField
+                    margin="dense"
+                    id="itemSelect"
+                    value=""
+                    label="Category"
+                    onChange={handleChange}
+                    select
+                  >
+                    <MenuItem name="category" value="fish">
+                      Fish
+                    </MenuItem>
+                    <MenuItem name="category" value="bugs">
+                      Bugs
+                    </MenuItem>
+                    <MenuItem name="category" value="sea">
+                      Sea Creatures
+                    </MenuItem>
+                    <MenuItem name="category" value="art">
+                      Art
+                    </MenuItem>
+                    <MenuItem name="category" value="recipes">
+                      Recipes
+                    </MenuItem>
+                  </TextField>
+                </Grid>
+                <Grid item>
+                  <TextField
+                    type="search"
+                    id="itemText"
+                    label="Item name"
+                    value=""
+                    name="searchTerm"
+                    onChange={handleChange}
+                  ></TextField>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
       </main>
