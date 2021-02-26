5e655f882ed8d67a045f9228a8d13f22cba4eee1
KHallam <kjhallam321@gmail.com>
2021-02-26 11:40:42 -0600

    added font to button component

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import "./Button.css";
 function BtnStyle(props) {
   return (
     <div className="buttons">
-      <button {...props} className="btn-hover color-1">
+      <button {...props} className="btn-hover color-1" style={{ fontFamily: 'FinkHeavy'}}>
         SUBMIT
       </button>
     </div>
