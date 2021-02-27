819a63ba6cde8aa88f7c8c334b3492c31a193e50
Michelle McConville <mchelmconvl@gmail.com>
2021-02-27 14:37:50 -0600

    added background color to input fields

--- client/src/pages/Items.jsx
+++ client/src/pages/Items.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Card, CardActions, CardContent, CardMedia, Container, FormControl, Grid, InputLabel, Link, MenuItem, TextField, Typography, Select } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import BtnStyle from "../components/Button/Button";
@@ -50,21 +50,13 @@ function Items() {
             <div className={classes.heroSearch}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
-                  <FormControl variant="outlined" className={classes.formControl} style={{ width: "25vw" }}>
+                  <FormControl variant="outlined" className={classes.formControl} style={{ backgroundColor: "mintcream", width: "25vw" }}>
                     <InputLabel id="demo-simple-select-outlined-label">Category</InputLabel>
                     <Select labelId="demo-simple-select-outlined-label" id="demo-simple-select-outlined" onChange={handleChange} value={searchState.category} label="Category" name="category">
-                      <MenuItem name="category" value="fish">
-                        Fish
-                      </MenuItem>
-                      <MenuItem name="category" value="bugs">
-                        Bugs
-                      </MenuItem>
-                      <MenuItem name="category" value="sea">
-                        Sea Creatures
-                      </MenuItem>
-                      <MenuItem name="category" value="art">
-                        Art
-                      </MenuItem>
+                      <MenuItem name="category" value="fish">Fish</MenuItem>
+                      <MenuItem name="category" value="bugs">Bugs</MenuItem>
+                      <MenuItem name="category" value="sea">Sea Creatures</MenuItem>
+                      <MenuItem name="category" value="art">Art</MenuItem>
                       <MenuItem name="category" value="recipes">
                         Recipes
                       </MenuItem>
@@ -72,7 +64,7 @@ function Items() {
                   </FormControl>
                 </Grid>
                 <Grid item>
-                  <TextField label="Item name" name="searchTerm" onChange={handleChange} type="search" value={searchState.searchTerm} variant="outlined" style={{ width: "25vw" }}></TextField>
+                  <TextField label="Item name" name="searchTerm" onChange={handleChange} type="search" value={searchState.searchTerm} variant="outlined" style={{  backgroundColor: "mintcream", width: "25vw" }}></TextField>
                   <BtnStyle onClick={handleClick} variant="contained"></BtnStyle>
                 </Grid>
               </Grid>
