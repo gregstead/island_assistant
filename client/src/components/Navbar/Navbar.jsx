de67baea301907225fc0bd759bdfe045493a5c3e
Michelle McConville <mchelmconvl@gmail.com>
2021-02-27 15:41:33 -0600

    cleaned up code formatting

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import axios from "axios";
 import clsx from 'clsx';
 import { Link } from 'react-router-dom'
-import { Avatar, AppBar, Button, CssBaseline, Divider, Drawer, List, makeStyles, useTheme, Toolbar, Typography } from '@material-ui/core';
+import { Avatar, AppBar, CssBaseline, Divider, Drawer, List, makeStyles, useTheme, Toolbar, Typography } from '@material-ui/core';
 import { IconButton, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import CategoryIcon from '@material-ui/icons/Category';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import MenuIcon from '@material-ui/icons/Menu';
-import HomeIcon from '@material-ui/icons/Home';
-import FilterVintageRoundedIcon from '@material-ui/icons/FilterVintageRounded';
-import CategoryIcon from '@material-ui/icons/Category';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import FilterVintageRoundedIcon from '@material-ui/icons/FilterVintageRounded';
+import HomeIcon from '@material-ui/icons/Home';
+import MenuIcon from '@material-ui/icons/Menu';
 import PersonAddRoundedIcon from '@material-ui/icons/PersonAddRounded';
 import PeopleIcon from '@material-ui/icons/People';
 import logo from "../../images/igLogo4.png";
-
 
 const drawerWidth = 170;
 
@@ -93,28 +93,36 @@ export default function MiniDrawer() {
   return (
     <div className={classes.root}>
       <CssBaseline />
+
       <AppBar
         className={clsx(classes.appBar, {[classes.appBarShift]: open})}
         position="fixed"
       >
-        <Toolbar className="styleBar" style={{ backgroundImage: "linear-gradient(30DEG, #017c74, #7cc9c3, #ef785a, #febdc3, #f5c24c, #fcea64)", height: "68px" }}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
+        <Toolbar 
+          className="styleBar" 
+          style={{ backgroundImage: "linear-gradient(30DEG, #017c74, #7cc9c3, #ef785a, #febdc3, #f5c24c, #fcea64)", height: "68px" }}
+        >
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          className={clsx(classes.menuButton, {
+            [classes.hide]: open,
+          })}
+        >
+
+        <MenuIcon />
+
+        </IconButton>
           <Typography variant="h6" noWrap>
-          <img src={logo} alt="Island Guru logo" className={classes.logo} style={{ width: "100%",  marginTop: "50px" }}/>
+            <img src={logo} alt="Island Guru logo" className={classes.logo} style={{ width: "100%",  marginTop: "50px" }}/>
           </Typography>
-          {/* <Button color="inherit" style={{}}>Login</Button> */}
+
         </Toolbar>
+
       </AppBar>
+
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
@@ -129,35 +137,40 @@ export default function MiniDrawer() {
         }}
         style={{background: '#88c9a1'}}
       >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Login', 'Register', 'Home', 'Items', 'Flowers', 'Villagers', 'Logout'].map((text, index) => (
-            <Link to={`/${text.toLowerCase()}`} style={{ color: '#017c74', textDecoration: 'none', fontWeight: "bold" }}>
-              <ListItem button key={text} style={{ paddingLeft: "18px", paddingRight: "23px" }}>
 
-                <ListItemIcon>
-                <Avatar style={{background: "#EDEDED", boxShadow: "1px 3px 1px #9E9E9E"}}>
-                {index === 0 && <AccountCircleIcon  key={"login"} style={{color: '#786951'}} />}
-                {index === 1 && <PersonAddRoundedIcon   key={"register"} style={{color: '#017c74'}} />}
-                {index === 2 && <HomeIcon  key={"home"} style={{color: '#7cc9c3'}} />}
-                {index === 3 && <CategoryIcon  key={"items"} style={{color: '#ef785a'}} />}
-                {index === 4 && <FilterVintageRoundedIcon  key={"flowers"} style={{color: '#febdc3'}} />}
-                {index === 5 && <PeopleIcon  key={"villagers"} style={{color: '#f5c24c'}} />}
-                {index === 6 && <ExitToAppIcon key={"exit"} onClick={async() => {
-                          await axios.get('/logout')
-                }} style={{color: '#786951', transform: "rotate(96)" }} />}
-                </Avatar>
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            </Link>
-          ))}
-        </List>
+      <div className={classes.toolbar}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </IconButton>
+      </div>
+
+      <Divider />
+
+      <List>
+        {['Login', 'Register', 'Home', 'Items', 'Flowers', 'Villagers', 'Logout'].map((text, index) => (
+          <Link to={`/${text.toLowerCase()}`} style={{ color: '#017c74', textDecoration: 'none', fontWeight: "bold" }}>
+
+            <ListItem button key={text} style={{ paddingLeft: "18px", paddingRight: "23px" }}>
+
+            <ListItemIcon>
+              <Avatar style={{background: "#EDEDED", boxShadow: "1px 3px 1px #9E9E9E"}}>
+                {index === 0 && <AccountCircleIcon        key={"login"}     style={{color: '#786951'}} />}
+                {index === 1 && <PersonAddRoundedIcon     key={"register"}  style={{color: '#017c74'}} />}
+                {index === 2 && <HomeIcon                 key={"home"}      style={{color: '#7cc9c3'}} />}
+                {index === 3 && <CategoryIcon             key={"items"}     style={{color: '#ef785a'}} />}
+                {index === 4 && <FilterVintageRoundedIcon key={"flowers"}   style={{color: '#febdc3'}} />}
+                {index === 5 && <PeopleIcon               key={"villagers"} style={{color: '#f5c24c'}} />}
+                {index === 6 && <ExitToAppIcon            key={"exit"}      style={{color: '#786951', transform: "rotate(96)" }}
+                onClick={async() => {await axios.get('/logout')}} />}
+              </Avatar>
+
+            </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+
+          </Link>
+        ))}
+      </List>
 
       </Drawer>
 
