24d43f1596739e9f6be5a2f4a8e35dd223e2da7e
Greg Stead <gregstead@gmail.com>
2021-02-28 18:14:20 -0600

    adds names for form fields

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -6,7 +6,11 @@ import API from "../../utils/API";
 
 const useStyles = makeStyles((theme) => ({
   root: { display: "flex", flexWrap: "wrap" },
-  textField: { marginLeft: theme.spacing(1), marginRight: theme.spacing(1), width: "25ch" },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "25ch",
+  },
 }));
 
 export default function RegisterTextFields() {
@@ -19,12 +23,13 @@ export default function RegisterTextFields() {
     friendCode: "",
     dreamAddress: "",
   });
-  
+
   const classes = useStyles();
 
   function handleSubmit(e) {
     e.preventDefault();
-    API.userRegister(registerState);
+    console.log(registerState);
+    // API.userRegister(registerState);
   }
 
   function handleChange(e) {
@@ -38,55 +43,76 @@ export default function RegisterTextFields() {
   return (
     // <div className={classes.root}>
     <Container maxWidth="sm">
-      <Box style={{ fontFamily: "FinkHeavy", fontSize: "50px", height: "50px", marginBottom: "30px", marginTop: "90px", textAlign: "center" }}>Registration</Box>
+      <Box
+        style={{
+          fontFamily: "FinkHeavy",
+          fontSize: "50px",
+          height: "50px",
+          marginBottom: "30px",
+          marginTop: "90px",
+          textAlign: "center",
+        }}
+      >
+        Registration
+      </Box>
 
       {/* <Image src="../../images/welcome.jpg" style={{ border: "solid", height: "50%", marginBottom: "20px" }} /> */}
-      
+
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="First Name"
+        name="firstName"
         onChange={handleChange}
         placeholder="First Name"
         variant="outlined"
         style={{ backgroundColor: "mintcream" }}
       />
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="Last Name"
+        name="lastName"
         onChange={handleChange}
         placeholder="Last Name"
         variant="outlined"
         style={{ backgroundColor: "mintcream" }}
       />
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="Username"
+        name="username"
         onChange={handleChange}
         placeholder="Username"
         variant="outlined"
         style={{ backgroundColor: "mintcream" }}
       />
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="Email"
+        name="email"
         onChange={handleChange}
         placeholder="Email"
         variant="outlined"
         style={{ backgroundColor: "mintcream" }}
       />
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="Password"
+        name="password"
         onChange={handleChange}
         placeholder="Password"
         type="password"
@@ -94,20 +120,24 @@ export default function RegisterTextFields() {
         style={{ backgroundColor: "mintcream" }}
       />
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="Friend Code"
+        name="friendCode"
         onChange={handleChange}
         placeholder="Friend Code"
         variant="outlined"
         style={{ backgroundColor: "mintcream" }}
       />
       <TextField
-        fullWidth margin="normal"
+        fullWidth
+        margin="normal"
         helperText=""
         InputLabelProps={{ shrink: true }}
         label="Dream Address"
+        name="dreamAddress"
         onChange={handleChange}
         placeholder="Dream Address"
         variant="outlined"
@@ -115,7 +145,6 @@ export default function RegisterTextFields() {
       />
 
       <BtnStyle onClick={handleSubmit} variant="contained"></BtnStyle>
-
     </Container>
     // </div>
   );
