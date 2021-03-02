28f70d42f47e4045c9b36ddc4e39a1a21f93efba
Michelle McConville <mchelmconvl@gmail.com>
2021-03-01 22:07:32 -0600

    moved styles into pages.css

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Box, Container, makeStyles, TextField } from "@material-ui/core";
-import Image from "material-ui-image";
+// import Image from "material-ui-image";
 import BtnStyle from "../Button/Button";
 import API from "../../utils/API";
+import "../../pages/pages.css"
 
 const useStyles = makeStyles((theme) => ({
   root: { display: "flex", flexWrap: "wrap" },
@@ -53,22 +54,12 @@ export default function RegisterTextFields() {
   return (
     // <div className={classes.root}>
     <Container maxWidth="sm">
-      <Box
-        style={{
-          fontFamily: "FinkHeavy",
-          fontSize: "50px",
-          height: "50px",
-          marginBottom: "30px",
-          marginTop: "90px",
-          textAlign: "center",
-        }}
-      >
-        Registration
-      </Box>
+      <Box id="page-header">Registration</Box>
 
       {/* <Image src="../../images/welcome.jpg" style={{ border: "solid", height: "50%", marginBottom: "20px" }} /> */}
 
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -78,9 +69,9 @@ export default function RegisterTextFields() {
         onChange={handleChange}
         placeholder="First Name"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -90,9 +81,9 @@ export default function RegisterTextFields() {
         onChange={handleChange}
         placeholder="Last Name"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -102,9 +93,9 @@ export default function RegisterTextFields() {
         onChange={handleChange}
         placeholder="Username"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -114,9 +105,9 @@ export default function RegisterTextFields() {
         onChange={handleChange}
         placeholder="Email"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -127,9 +118,9 @@ export default function RegisterTextFields() {
         placeholder="Password"
         type="password"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -139,9 +130,9 @@ export default function RegisterTextFields() {
         onChange={handleChange}
         placeholder="Friend Code"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
       <TextField
+        id="register-input"
         fullWidth
         margin="normal"
         helperText=""
@@ -151,7 +142,6 @@ export default function RegisterTextFields() {
         onChange={handleChange}
         placeholder="Dream Address"
         variant="outlined"
-        style={{ backgroundColor: "mintcream" }}
       />
 
       <BtnStyle onClick={handleSubmit} variant="contained"></BtnStyle>
