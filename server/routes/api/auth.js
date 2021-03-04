c69c9f8ab153cae7b6f32fd22c3c173dbc082a9d
Greg Stead <gregstead@gmail.com>
2021-03-04 07:05:05 -0600

    fixes for linter and formatter

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -7,9 +7,9 @@ router.use(passport.authenticate('local'));
 // Matches with '/api/auth/login'
 router.route('/login').post(authController.login);
 
-router.get('/logout', (req,res) => {
-    req.logout();
-    res.redirect('/')
-  })
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
